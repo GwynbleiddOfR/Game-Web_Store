164ca00b49284fab0f72b9ea66e38e15079083b4
GwynbleiddOfR <se.apileo@duocuc.cl>
2024-06-09 06:07:34 -0400

    validar nombre por el lado del servidor

--- gamewebstore/forms.py
+++ gamewebstore/forms.py
@@ -1,9 +1,21 @@
 from django import forms
 from .models import *
 from .enumeraciones import *
+from django.core.exceptions import ValidationError
+import re
 
 class UsuarioForm(forms.ModelForm):
+
+
     
     class Meta:
         model = Usuario
         fields = ['nombre', 'apellido', 'nomb_usuario', 'sexo', 'correo', 'telefono', 'ciudad', 'direccion', 'contraseña', 'fecha_creacion']
+
+        nombre = forms.CharField()
+
+    def clean_nombre(self):
+        data = self.cleaned_data['nombre']
+        if not re.match("^[A-Za-z]*$", data):
+            raise ValidationError("Este campo solo debe contener letras.")
+        return data
